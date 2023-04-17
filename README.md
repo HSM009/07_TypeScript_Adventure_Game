3ce11ab12c4d3f7c08e90e300719780749af84f8
HSM009 <hoseinsirat@gmail.com>
2023-04-17 10:00:38 +0500

    intial setup commit

--- README.md
+++ README.md
@@ -2,8 +2,8 @@ Simple Adventure Game based CLI application using inquirer, chalk and chalk-anim
 
 To test application write following comands
 
-npm i hsm-currency-converter
+npm i hsm-adventure-game
 
-npx hsm-currency-converter
+npx hsm-adventure-game
 
 Coded By Hosein Sirat Mohammad
--- index.ts
+++ index.ts
@@ -4,16 +4,38 @@
 //IMPORTS HERE
 //------------------------------------------------------------------------------
 
+import inquirer from "inquirer";
+import chalk from "chalk"; 
+import chalkAnimation from "chalk-animation";
 //------------------------------------------------------------------------------
 //CLASSES/CONSTRUCTORS/OOPs HERE
 //------------------------------------------------------------------------------
+
+const stopTime = ()=>{
+    return new Promise((res:any)=>{
+        setTimeout(res,3500);
+    })
+};
+
+async function mainMenuFunc() {
+    
+};
 
 //------------------------------------------------------------------------------
 //FUNCTIONS HERE
 //------------------------------------------------------------------------------
 
+async function welcomeFunc(welcomeMessage:string ) {
+    let rainbowTitle = chalkAnimation.neon(chalk.blueBright("Welcome To "+ welcomeMessage +" App!\n\nCoded By Hosein Sirat Mohammad\n"));
+    await stopTime();
+    rainbowTitle.stop();
+};
+
 //------------------------------------------------------------------------------
 //MAIN HERE
 //------------------------------------------------------------------------------
 
-console.log('hi*');
+let appName:string = "Student Management System";
+await welcomeFunc(appName);
+
+await mainMenuFunc();
